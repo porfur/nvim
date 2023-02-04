596ba5859702b6d2615071338b1c50007dad6ecb
OP <op@OPs-MacBook-Pro.local>
2023-02-04 12:58:29 +0200

    Some custom keymaps

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -1,5 +1,5 @@
--- Functions 
-function OP_Toggle_Ex ()
+-- Functions
+function OP_Toggle_Ex()
 	if vim.bo.filetype == 'netrw'
 	-- Close Ex
 	then
@@ -10,9 +10,9 @@ function OP_Toggle_Ex ()
 	end
 end
 
-function OP_q_Ex ()
+function OP_q_Ex()
 	if vim.bo.filetype == 'netrw'
-		-- Close Ex
+	-- Close Ex
 	then
 		vim.cmd('Rex')
 	else
@@ -21,29 +21,53 @@ function OP_q_Ex ()
 	end
 end
 
--- Basic
+-----------------------------------------------------------------------------------
+--MY KEYMAPS
+-----------------------------------------------------------------------------------
+-- Escape with jk
 vim.keymap.set('i', 'kj', '<Esc>')
-vim.keymap.set({ 'n','v' }, '<leader>bs',':w<CR>',{desc='Save buffer'})
-vim.keymap.set({ 'n','v' }, '<C-s>' ,':w<CR>',{desc='Save buffer'})
-vim.keymap.set({ 'n','v' }, '<leader>bS',':wa<CR>',{desc='Save all buffer'})
-vim.keymap.set({ 'n','v' }, '<leader>bk',':bdelete<CR>',{desc='Kill buffer'})
 
---Debug
-vim.keymap.set({ 'n','v' }, '<leader>df',function ()
-print(vim.bo.filetype)
-end,{desc='Printfiletype'})
-vim.keymap.set({ 'n','v' }, '<leader>db', function ()
-local	b = vim.inspect( vim.api.nvim_list_bufs() )
+
+-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
+-- BUFFERS
+-----------------------------------------------------------------------------------
+-- TODO Male ot work with command - S
+--
+-- Save
+vim.keymap.set({ 'n', 'v' }, '<leader>bs', ':w<CR>', { desc = '[ :w ] Save Buffer' })
+vim.keymap.set({ 'n', 'v' }, '<leader>bS', ':wa<CR>', { desc = '[ :wa ] Save all Buffer' })
+vim.keymap.set({ 'n', 'v', 'i' }, '<C-s>', ':w<CR>', { desc = '[ :w ] Save Buffer' })
+vim.keymap.set({ 'n', 'v', 'i' }, '<C-S>', ':wa<CR>', { desc = '[ :wa ] Save all Buffer' })
+
+-- Close
+   vim.keymap.set({ 'n', 'v' }, '<leader>bc', ':bd<CR>', { desc = '[ :bd ] Delete Buffer' })
+vim.keymap.set({ 'n', 'v' }, '<leader>bC', ':%bd<CR>', { desc = '[ :%bd ] Delete All Buffers' })
+vim.keymap.set({ 'n', 'v' }, '<leader>bk', ':bd!<CR>', { desc = '[ :bd! ] Kill Buffer' })
+vim.keymap.set({ 'n', 'v' }, '<leader>bK', ':%bd!<CR>', { desc = '[ :%bd! ] Kill All Buffers' })
+
+--Debun
+vim.keymap.set({ 'n', 'v' }, '<leader>df', function()
+	print(vim.bo.filetype)
+end, { desc = 'Printfiletype' })
+vim.keymap.set({ 'n', 'v' }, '<leader>db', function()
+	local b = vim.inspect(vim.api.nvim_list_bufs())
 	print(b)
-end,{desc='Esc Quickfix'})
+end, { desc = 'Esc Quickfix' })
 --Netrw Keymaps
-vim.keymap.set({  'n','v','t'  }, '<leader>o-', OP_Toggle_Ex, {desc="Open Netrw"})
+vim.keymap.set({ 'n', 'v', 't' }, '<leader>o-', OP_Toggle_Ex, { desc = "Open Netrw" })
 
 --NeoTree Keymaps
-vim.keymap.set({  'n','v','t'  }, '<leader>op',':NeoTreeFloatToggle<CR>', {desc="Open NeoTreeFloat"})
+vim.keymap.set({ 'n', 'v', 't' }, '<leader>op', ':NeoTreeFloatToggle<CR>', { desc = "Open NeoTreeFloat" })
 
--- vim.keymap.set({'n','v','t'},'<C-w>j',':resize -5<CR>',{})
--- vim.keymap.set({'n','v','t'},'<leader>k',':resize +5<CR>',{})
+--Split resize
+vim.keymap.set({ 'n', 'v', 't' }, '–', ':resize -5<CR>', {}) -- Alt -
+vim.keymap.set({ 'n', 'v', 't' }, '≠', ':resize +5<CR>', {}) -- Alt +
+vim.keymap.set({ 'n', 'v', 't' }, '—', ':vertical resize -5<CR>', {}) -- Alt Shift -
+vim.keymap.set({ 'n', 'v', 't' }, '±', ':vertical resize +5<CR>', {}) -- Alt Shift +
+
+--Lazygit
+vim.keymap.set({ 'n', 'v' }, '<leader>gg', ':LazyGit<CR>', {})
 
 --Telescope Keymap (already in kickstart.nvim)
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
@@ -53,8 +77,14 @@ vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
 --Close help and quickfix files with escape
-vim.keymap.set({ 'n','v' }, '<Esc>', function ()
-if vim.bo.filetype == 'qf'
-	or vim.bo.filetype == 'help'
+vim.keymap.set({ 'n', 'v' }, '<Esc>', function()
+	if vim.bo.filetype == 'qf'
+			or vim.bo.filetype == 'help'
 	then vim.cmd('bdelete') end
-end,{desc='Esc Quickfix'})
+end, { desc = 'Esc Quickfix' })
+
+--Tmux Navigator
+vim.keymap.set('n', '˙', ':<C-U>TmuxNavigateLeft<cr>', { desc = 'Tmux Navigate Left' })
+vim.keymap.set('n', '∆', ':<C-U>TmuxNavigateDown<cr>', { desc = 'Tmux Navigate Down' })
+vim.keymap.set('n', '˚', ':<C-U>TmuxNavigateUp<cr>', { desc = 'Tmux Navigate Up' })
+vim.keymap.set('n', '¬', ':<C-U>TmuxNavigateRight<cr>', { desc = 'Tmux Navigate Right' })
