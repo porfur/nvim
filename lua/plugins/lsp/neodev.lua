78d0ac31c52634020f6af5c9a35e50ded47bb775
OP <ovidiu.ps@gmail.com>
2024-01-03 15:20:39 +0200

    asd

--- lua/plugins/lsp/neodev.lua
+++ lua/plugins/lsp/neodev.lua
@@ -1,5 +1,4 @@
 -- Automatically configures lua-language-server for your Neovim config, Neovim runtime and plugin directories
 -- https://github.com/folke/neodev.nvim
 return { 
-	-- enable = false, -- Uncomment to disable
 	"folke/neodev.nvim", opts = {} }
