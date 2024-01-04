d0b3b4e1de1f4bc9f347202d0d9c986cd9ece50f
OP <ovidiu.ps@gmail.com>
2024-01-04 23:56:08 +0200

    Broken cmp

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,11 +1,178 @@
--- Order all lsp related plugins
--- The order of these items is important
--- Check out the link for the documentation
--- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/lazy-loading-with-lazy-nvim.md
+-- note: diagnostics are not exclusive to lsp servers
+-- so these can be global keybindings
+vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
 
-return{
-  require('plugins.lsp.lspzero') ,
-  require('plugins.lsp.mason') ,
-  require('plugins.lsp.cmp') ,
-  require('plugins.lsp.nvim-lsp-config') ,
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = { buffer = event.buf }
+    -- these will be buffer-local keybindings
+    -- because they only work if you have an active language server
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+  end
+})
+
+local kind_icons = {
+  Text = '',
+  Method = '',
+  Function = '',
+  Constructor = '',
+  Field = '',
+  Variable = '',
+  Class = '',
+  Interface = '',
+  Module = '',
+  Property = '',
+  Unit = '',
+  Value = '',
+  Enum = '',
+  Keyword = '',
+  Snippet = '',
+  Color = '',
+  File = '',
+  Reference = '',
+  Folder = '',
+  EnumMember = '',
+  Constant = '',
+  Struct = '',
+  Event = '',
+  Operator = '',
+  TypeParameter = '',
+}
+local check_backspace = function()
+  local col = vim.fn.col '.' - 1
+  return col == 0 or vim.fn.getline('.'):sub(col, col):match '%s'
+end
+
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "L3MON4D3/LuaSnip",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    'hrsh7th/cmp-buffer',       -- Optional
+    'hrsh7th/cmp-path',         -- Optional
+    'saadparwaiz1/cmp_luasnip', -- Optional
+    'hrsh7th/cmp-nvim-lua',     -- Optional
+    'hrsh7th/cmp-nvim-lsp-signature-help',
+    "rafamadriz/friendly-snippets"
+
+  },
+  config = function()
+    local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+    local default_setup = function(server)
+      require('lspconfig')[server].setup({
+        capabilities = lsp_capabilities,
+      })
+    end
+
+    require('mason').setup({})
+    require('mason-lspconfig').setup({
+      ensure_installed = {},
+      handlers = { default_setup },
+    })
+
+    local luasnip = require('luasnip')
+    require('luasnip/loaders/from_vscode').lazy_load()
+    local cmp = require('cmp')
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body) -- For `luasnip` users.
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+
+        -- `Enter` key to confirm completion
+        ['<CR>'] = cmp.confirm({ select = true }),
+        -- Ctrl+Space to trigger completion menu
+        ['<C-Space>'] = cmp.complete(),
+        -- Navigate between snippet placeholder
+        -- FIXME
+        ['<C-n>'] = luasnip.jump(1),
+        ['<C-p>'] = luasnip.jump(-1),
+        -- Scroll up and down in the completion documentation
+        ['<C-u>'] = cmp.scroll_docs(-1),
+        ['<C-d>'] = cmp.scroll_docs(1),
+        -- Scroll to next and prev item in list
+        ['<C-k>'] = cmp.select_prev_item(),
+        ['<C-j>'] = cmp.select_next_item(),
+        -- Super tab
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expandable() then
+            luasnip.expand()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif check_backspace() then
+            fallback()
+          else
+            fallback()
+          end
+        end, {
+          'i',
+          's',
+        }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, {
+          'i',
+          's',
+        }),
+      }),
+      formatting = {
+        expandable_indicator = true,
+        fields = { 'kind', 'abbr', 'menu' },
+        format = function(entry, vim_item)
+          vim_item.kind = kind_icons[vim_item.kind]
+          vim_item.menu = ({
+            nvim_lsp = '[LSP]',
+            luasnip = '[LuaSnip]',
+            nvim_lua = '[nvim_lua]',
+            buffer = '[buffer]',
+            path = '[path]',
+            nvim_lsp_signature_help = '[LSP-Signature]',
+          })[entry.source.name]
+          return vim_item
+        end,
+      },
+      sources = {
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+        { name = 'buffer' },
+        { name = 'path' },
+        { name = 'nvim_lsp_signature_help' },
+        { name = 'luasnip' },
+      },
+      select_behavior = 'insert',
+      experimental = {
+        ghost_text = false,
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+    })
+  end
 }
