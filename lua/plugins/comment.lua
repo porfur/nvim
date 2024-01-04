619733e161895cac5c10201a48758b9b52224eb9
OP <ovidiu.ps@gmail.com>
2024-01-05 00:27:11 +0200

    Fixed Comment

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -3,11 +3,15 @@
 
 return {
   'numToStr/Comment.nvim',
-  lazy = false,
   dependencies = {
     --https://github.com/JoosepAlviste/nvim-ts-context-commentstring
     'JoosepAlviste/nvim-ts-context-commentstring'
   },
   -- TODO: Figure out how to use this pre hook with lazy
+  config = function()
+    require('Comment').setup({
+      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
+    })
+  end
   -- config=function() pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook() end,
 }
