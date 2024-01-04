72d107595e6c8bba13bd4cb2be4c9e6f2594e9d8
OP <ovidiu.ps@gmail.com>
2024-01-04 12:01:26 +0200

    added lsp keymaps

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,7 +1,7 @@
 -- https://github.com/windwp/nvim-autopairs
 -- Close pairs of parantheses automatically
- return {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    opts = {} -- this is equalent to setup({}) function
+return {
+  'windwp/nvim-autopairs',
+  event = 'InsertEnter',
+  opts = {}, -- this is equalent to setup({}) function
 }
