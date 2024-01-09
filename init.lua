893f9d41033852148be9f3a514f78f17bdc86ddc
OP <ovidiu.ps@gmail.com>
2024-01-09 11:02:13 +0200

    WIP Keymap

--- init.lua
+++ init.lua
@@ -1,7 +1,216 @@
+--[[ OPTIONS START ]]
+-- TODO REMOVE UNUSED
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.opt.backup = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cmdheight = 1
+vim.opt.completeopt = { "menuone", "noselect" }
+vim.opt.conceallevel = 0
+vim.opt.fileencoding = "utf-8"
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.mouse = "a"
+-- vim.opt.showmode = false
+vim.opt.showtabline = 1
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.swapfile = false
+vim.opt.termguicolors = true
+vim.opt.timeoutlen = 300
+vim.opt.undofile = true
+vim.opt.updatetime = 250
+vim.opt.writebackup = false
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.laststatus = 3
+vim.opt.showcmd = false
+vim.opt.ruler = false
+vim.opt.numberwidth = 4
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+vim.opt.scrolloff = 5
+vim.opt.sidescrolloff = 5
+vim.opt.fillchars.eob=" "
+vim.opt.linebreak = true
+vim.opt.foldmethod = "indent"
+vim.opt.foldenable = false
+vim.opt.guifont = "Iosevka:h28"
 
-require('config')
+--[[ OPTIONS END ]]
+
+
+--[[ AUTOCOMMANDS START ]]
+--TODO figure out when these are usefull
+local autocmd = vim.api.nvim_create_autocmd
+autocmd({ "FileType" },{
+    pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
+    callback = function()
+      vim.cmd [[ nnoremap <silent> <buffer> q :close<CR> set nobuflisted
+    ]]
+    end,
+  })
+
+autocmd({ "FileType" }, {
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
+
+autocmd({ "VimResized" }, {
+  callback = function()
+    vim.cmd "tabdo wincmd ="
+  end,
+})
+
+autocmd({ "CmdWinEnter" }, {
+  callback = function()
+    vim.cmd "quit"
+  end,
+})
+
+autocmd({ "TextYankPost" }, {
+  callback = function()
+    vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+  end,
+})
+
+autocmd({ "BufWritePost" }, {
+  pattern = { "*.java" },
+  callback = function()
+    vim.lsp.codelens.refresh()
+  end,
+})
+
+autocmd({ "VimEnter" }, {
+  callback = function()
+    vim.cmd "hi link illuminatedWord LspReferenceText"
+  end,
+})
+
+autocmd({ "BufWinEnter" }, {
+  callback = function()
+    local line_count = vim.api.nvim_buf_line_count(0)
+    if line_count >= 5000 then
+      vim.cmd "IlluminatePauseBuf"
+    end
+  end,
+})
+--[[ AUTOCOMMANDS END ]]
+
+--[[ KEYMAPS START ]]
+
+-- MODES --
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+--   all = "",
+
+-- KEYMAP LOCALS --
+local key = vim.keymap.set
+local opts = function(desc)
+  return { silent = false, desc = desc or '**No Description**' }
+end
+
+-- BINDINGS --
+-- Disable Single Space
+key('', '<Space>', '<Nop>', opts 'Space')
+
+-- Center cursor when moving pages
+key("n", "<C-d>", "<C-d>zz")
+key("n", "<C-u>", "<C-u>zz")
+key("n", "<C-f>", "<C-f>zz")
+key("n", "<C-b>", "<C-b>zz")
+
+-- Better window navigation
+key('n', '<C-h>', '<C-w>h', opts 'Window Navigate Left')
+key('n', '<C-j>', '<C-w>j', opts 'Window Navigate Down')
+key('n', '<C-k>', '<C-w>k', opts 'Window Navigate Up')
+key('n', '<C-l>', '<C-w>l', opts 'Window Navigate Right')
+
+-- Resize with Alt-hjkl
+key('n', '˚', ':resize -2<CR>', opts 'Window Horizontal Shrink')
+key('n', '∆', ':resize +2<CR>', opts 'Window Horizontal Grow')
+key('n', '˙', ':vertical resize -2<CR>', opts 'Window Vertical Shrink')
+key('n', '¬', ':vertical resize +2<CR>', opts 'Window Vertical Grow')
+key('n', '≠', '<C-w>=', opts 'Equalize Windows')
+
+--Tabs
+key('n', '<leader>tn', ':tabnew<CR>', opts 'Tab New')
+key('n', '<leader>tc', ':tabclose<CR>', opts 'Tab Close')
+key('n', '<leader>to', ':tabonly<CR>', opts 'Tab Only')
+
+-- Navigate buffers
+key('n', '<C-n>', ':bnext<CR>', opts 'Buffer Next')
+key('n', '<C-p>', ':bprevious<CR>', opts 'Buffer Previous')
+key('n', '<C-6>', '<C-^>', opts 'Alternate Buffer')
+key('n', '<leader>bn', ':bnext<CR>', opts 'Buffer Next')
+key('n', '<leader>bp', ':bprevious<CR>', opts 'Buffer Previous')
+key('n', '<leader>ba', '<C-^>', oet nobuflistedts 'Alternate Buffer')
+
+--Save buffers
+key({ 'n', 'v' }, '<leader>bs', ':write<CR>', opts 'Write Buffer')
+key({ 'n', 'v' }, 'ZS', ':write<CR>', opts 'Write Buffer')
+key({ 'n', 'v' }, '<leader>bS', ':wall<CR>', opts 'Write all Buffers')
+key({ 'n', 'v' }, '<leader>bw', ':write<CR>', opts 'Write Buffer')
+key({ 'n', 'v' }, '<leader>bW', ':wall<CR>', opts 'Write all Buffers')
+key({ 'n', 'v' }, '<C-s>', ':write<CR>', opts 'Write Buffer')
+key({ 'n', 'v' }, '<C-S>', ':wall<CR>', opts 'Write all Buffers')
+
+-- Close buffers
+-- TODO SIMPLIFY THIS USING THE Z command
+-- switch to prev buffer then delete prev buffer [ bdelete# ]
+-- That way splits dont close
+key('n', '<leader>bd', ':bprevious<bar>bdelete#!<CR>', opts 'Delete Buffer (Discard Changes)')
+key('n', '<leader>bD', ':bprevious<bar> %bdelete!<CR>', opts  'Delete All Buffer (Discard Changes)')
+key('n', '<leader>bc', ':write<bar>bprevious<bar>bdelete#<CR>', opts 'Write & Delete Buffer')
+key('n', '<leader>bC', ':wall<bar> %bdelete<CR>', opts 'Write & Delete Buffers')
+key('n', '<leader>bqq<CR>', ':qa!<CR>', opts  'Quit All Buffers')
+
+-- Clear highlights
+key('n', '<leader>x', ':nohlsearch<cr>', opts 'clear search highlight')
+
+-- Paste in visual mode doesn't polute the clipboard with the old selection
+key('v', 'p', '"_dp', opts())
+
+-- press jk fast to esc
+key('i', 'kj', '<esc>', opts())
+
+-- visual --
+-- stay in indent mode
+key('v', '<', '<gv', opts())
+key('v', '>', '>gv', opts())
+
+-- Netrw
+key('n', '<leader>o-', vim.cmd.Ex, opts 'Open Netrw')
+
+-- Move line in visual mode
+key('v', 'J', ":m '>+1<CR>gv=gv")
+key('v', 'K', ":m '<-2<CR>gv=gv")
+
+--Insert New Line in normal mode
+key('n', '[o', 'moO<ESC>`o', opts 'Empty Line above')
+key('n', ']o', 'moo<ESC>`o', opts 'Empty Line below')
+
+-- Split Line
+key('n', '<Enter>', 'i<CR><Esc>', opts 'Split line (Opposite of J)')
+
+-- Sessions
+key('n', '<leader>qs', ':mksession!<CR>', opts 'Make session')
+key('n', '<leader>ql', ':source Session.vim<CR>', opts 'Make load')
+
+--[[ KEYMAPS END ]]
 -- [[Lazy setup]]
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,14 +1,12 @@
--- Shorten function name
-local key = vim.keymap
-
--- Silent keymap option & Desc
+--[[ FUNCTIONS START ]]
+local key = key
 local opts = function(desc)
-  return { silent = false, desc = desc or 'NEED DESC' }
+  return { silent = false, desc = desc or '**No Description**' }
 end
 
+--[[ FUNCTIONS END]]
 --Remap space as leader key
-key.set('', '<Space>', '<Nop>', opts 'Space')
-vim.g.mapleader = ' '
+key('', '<Space>', '<Nop>', opts 'Space')
 
 -- Modes
 --   normal_mode = "n",
@@ -19,118 +17,110 @@ vim.g.mapleader = ' '
 --   command_mode = "c",
 
 --Primagen
--- vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
--- vim.keymap.set("n", "n", "nzzzv")
--- vim.keymap.set("n", "N", "Nzzzv")
+-- key("n", "J", "mzJ`z")
+key("n", "<C-d>", "<C-d>zz")
+key("n", "<C-u>", "<C-u>zz")
+-- key("n", "n", "nzzzv")
+-- key("n", "N", "Nzzzv")
 --
 -- Better window navigation
-key.set('n', '<C-h>', '<C-w>h', opts 'Window Navigate Left')
-key.set('n', '<C-j>', '<C-w>j', opts 'Window Navigate Down')
-key.set('n', '<C-k>', '<C-w>k', opts 'Window Navigate Up')
-key.set('n', '<C-l>', '<C-w>l', opts 'Window Navigate Right')
-key.set('n', '<C-c>', '<C-w>c', opts 'Close Window')
+key('n', '<C-h>', '<C-w>h', opts 'Window Navigate Left')
+key('n', '<C-j>', '<C-w>j', opts 'Window Navigate Down')
+key('n', '<C-k>', '<C-w>k', opts 'Window Navigate Up')
+key('n', '<C-l>', '<C-w>l', opts 'Window Navigate Right')
+key('n', '<C-c>', '<C-w>c', opts 'Close Window')
 -- Resize with Alt-hjkl
-key.set('n', '˚', '<cmd>resize -2<CR>', opts 'Window Horizontal Shrink')
-key.set('n', '∆', '<cmd>resize +2<CR>', opts 'Window Horizontal Grow')
-key.set('n', '˙', '<cmd>vertical resize -2<CR>', opts 'Window Vertical Shrink')
-key.set('n', '¬', '<cmd>vertical resize +2<CR>', opts 'Window Vertical Grow')
-key.set('n', '≠', '<C-w>=', opts 'Equalize Windows')
+key('n', '˚', '<cmd>resize -2<CR>', opts 'Window Horizontal Shrink')
+key('n', '∆', '<cmd>resize +2<CR>', opts 'Window Horizontal Grow')
+key('n', '˙', '<cmd>vertical resize -2<CR>', opts 'Window Vertical Shrink')
+key('n', '¬', '<cmd>vertical resize +2<CR>', opts 'Window Vertical Grow')
+key('n', '≠', '<C-w>=', opts 'Equalize Windows')
 --Leader window commands
-key.set('n', '<leader>wh', '<C-w>h', opts 'Window Navigate Left')
-key.set('n', '<leader>wj', '<C-w>j', opts 'Window Navigate Down')
-key.set('n', '<leader>wk', '<C-w>k', opts 'Window Navigate Up')
-key.set('n', '<leader>wl', '<C-w>l', opts 'Window Navigate Right')
-key.set('n', '<leader>wc', '<C-w>c', opts 'Close Window')
-key.set('n', '<leader>wr', '<C-w>r', opts 'Swap Windows')
-key.set('n', '<leader>wo', '<C-w>o', opts 'Only Window')
-key.set('n', '<leader>w=', '<C-w>=', opts 'Equalize Windows')
-key.set('n', '<leader>wv', '<C-w>v', opts 'Vertical Split')
-key.set('n', '<leader>ws', '<C-w>s', opts 'Horizontal Split')
+key('n', '<leader>wh', '<C-w>h', opts 'Window Navigate Left')
+key('n', '<leader>wj', '<C-w>j', opts 'Window Navigate Down')
+key('n', '<leader>wk', '<C-w>k', opts 'Window Navigate Up')
+key('n', '<leader>wl', '<C-w>l', opts 'Window Navigate Right')
+key('n', '<leader>wc', '<C-w>c', opts 'Close Window')
+key('n', '<leader>wr', '<C-w>r', opts 'Swap Windows')
+key('n', '<leader>wo', '<C-w>o', opts 'Only Window')
+key('n', '<leader>w=', '<C-w>=', opts 'Equalize Windows')
+key('n', '<leader>wv', '<C-w>v', opts 'Vertical Split')
+key('n', '<leader>ws', '<C-w>s', opts 'Horizontal Split')
 
 --Tabs
-key.set('n', '<leader>tn', '<cmd>tabnew<CR>', opts 'Tab New')
-key.set('n', '<leader>tc', '<cmd>tabclose<CR>', opts 'Tab Close')
-key.set('n', '<leader>to', '<cmd>tabonly<CR>', opts 'Tab Only')
+key('n', '<leader>tn', '<cmd>tabnew<CR>', opts 'Tab New')
+key('n', '<leader>tc', '<cmd>tabclose<CR>', opts 'Tab Close')
+key('n', '<leader>to', '<cmd>tabonly<CR>', opts 'Tab Only')
 
 -- Navigate buffers
-key.set('n', '<C-n>', '<cmd>bnext<CR>', opts 'Buffer Next')
-key.set('n', '<C-p>', '<cmd>bprevious<CR>', opts 'Buffer Previous')
-key.set('n', '<leader>bn', '<cmd>bnext<CR>', opts 'Buffer Next')
-key.set('n', '<leader>bp', '<cmd>bprevious<CR>', opts 'Buffer Previous')
-
+key('n', '<C-n>', ':bnext<CR>', opts 'Buffer Next')
+key('n', '<C-p>', ':bprevious<CR>', opts 'Buffer Previous')
+key('n', '<C-6>', '<C-^', opts 'Alternate Buffer')
+key('n', '<leader>bn', ':bnext<CR>', opts 'Buffer Next')
+key('n', '<leader>bp', ':bprevious<CR>', opts 'Buffer Previous')
+key('n', '<leader>ba', '<C-^', opts 'Buffer Previous')
 --Save buffers
-key.set({ 'n', 'v' }, '<leader>bs', '<cmd>write<CR>', opts 'Write Buffer')
-key.set({ 'n', 'v' }, '<leader>bS', '<cmd>wall<CR>', opts 'Write all Buffers')
-key.set({ 'n', 'v' }, '<leader>bw', '<cmd>write<CR>', opts 'Write Buffer')
-key.set({ 'n', 'v' }, '<leader>bW', '<cmd>wall<CR>', opts 'Write all Buffers')
-key.set({ 'n', 'v' }, '<C-s>', '<cmd>write<CR>', opts 'Write Buffer')
-key.set({ 'n', 'v' }, '<C-S>', '<cmd>wall<CR>', opts 'Write all Buffers')
+key({ 'n', 'v' }, '<leader>bs', '<cmd>write<CR>', opts 'Write Buffer')
+key({ 'n', 'v' }, '<leader>bS', '<cmd>wall<CR>', opts 'Write all Buffers')
+key({ 'n', 'v' }, '<leader>bw', '<cmd>write<CR>', opts 'Write Buffer')
+key({ 'n', 'v' }, '<leader>bW', '<cmd>wall<CR>', opts 'Write all Buffers')
+key({ 'n', 'v' }, '<C-s>', '<cmd>write<CR>', opts 'Write Buffer')
+key({ 'n', 'v' }, '<C-S>', '<cmd>wall<CR>', opts 'Write all Buffers')
 
 -- Close buffers
 -- switch to prev buffer then delete prev buffer [ bdelete# ]
 -- That way splits dont close
-key.set('n', '<leader>bD', '<cmd>bprevious<bar>bdelete!#<CR>', opts 'Delete Buffer (Discard Changes)')
-key.set('n', '<leader>bd', '<cmd>bprevious<bar>bdelete#<CR>', opts 'Delete Buffer')
-key.set('n', '<leader>bc', '<cmd>write<bar>bprevious<bar>bdelete#<CR>', opts 'Write & Delete Buffer')
-key.set('n', '<leader>bC', '<cmd>wall<bar> %bdelete<CR>', opts 'Write & Delete Buffers')
-key.set('n', '<leader>bqq', '<cmd>q!<CR>', opts 'Quit Buffer')
-key.set('n', '<leader>bQQ', '<cmd>qa!<CR>', opts 'Quit All Buffers')
+key('n', '<leader>bd', '<cmd>bprevious<bar>bdelete#!<CR>', opts 'Delete Buffer (Discard Changes)')
+key('n', '<leader>bD', '<cmd>bprevious<bar> %bdelete!<CR>', opts  'Delete All Buffer (Discard Changes)')
+key('n', '<leader>bc', '<cmd>write<bar>bprevious<bar>bdelete#<CR>', opts 'Write & Delete Buffer')
+key('n', '<leader>bC', '<cmd>wall<bar> %bdelete<CR>', opts 'Write & Delete Buffers')
+key('n', '<leader>bqq', '<cmd>q!<CR>', opts 'Quit Buffer')
+key('n', '<leader>bQQ', '<cmd>qa!<CR>', opts 'Quit All Buffers')
 
 -- Clear highlights
-key.set('n', '<leader>x', '<cmd>nohlsearch<cr>', opts 'clear search highlight')
+key('n', '<leader>x', '<cmd>nohlsearch<cr>', opts 'clear search highlight')
 
 -- better paste
-key.set('v', 'p', '"_dp', opts())
+key('v', 'p', '"_dp', opts())
 
 -- insert --
 -- press jk fast to esc
-key.set('i', 'kj', '<esc>', opts())
+key('i', 'kj', '<esc>', opts())
 
 -- visual --
 -- stay in indent mode
-key.set('v', '<', '<gv', opts())
-key.set('v', '>', '>gv', opts())
+key('v', '<', '<gv', opts())
+key('v', '>', '>gv', opts())
 
 -- Netrw
-key.set('n', '<leader>o-', vim.cmd.Ex, opts 'Open Netrw')
+key('n', '<leader>o-', vim.cmd.Ex, opts 'Open Netrw')
 
 -- Move line in visual mode
-key.set('v', 'J', ":m '>+1<CR>gv=gv")
-key.set('v', 'K', ":m '<-2<CR>gv=gv")
+key('v', 'J', ":m '>+1<CR>gv=gv")
+key('v', 'K', ":m '<-2<CR>gv=gv")
 
 --Insert New Line in normal mode
-key.set('n', '[o', 'moO<ESC>`o', opts 'Empty Line above')
-key.set('n', ']o', 'moo<ESC>`o', opts 'Empty Line below')
+key('n', '[o', 'moO<ESC>`o', opts 'Empty Line above')
+key('n', ']o', 'moo<ESC>`o', opts 'Empty Line below')
 
 -- Split Line
-key.set('n', '<Enter>', 'i<CR><Esc>', opts 'Split line (Opposite of J)')
+key('n', '<Enter>', 'i<CR><Esc>', opts 'Split line (Opposite of J)')
 
 -- Sessions
-key.set('n', '<leader>qs', ':mksession!<CR>', opts 'Make session')
-key.set('n', '<leader>ql', ':source Session.vim<CR>', opts 'Make load')
-
--- Plugins --
-
--- Git
-key.set('n', '<leader>gg', '<cmd>LazyGit<CR>', opts 'Lazy [G]it')
-key.set('n', '<leader>gb', '<cmd>GitBlameToggle<CR>', opts 'Toggle Git [B]lame')
-
---Expand snippet
-key.set('i', '<C-s>', '<Plug>luasnip-expand-snippet', opts 'Expand snippet')
+key('n', '<leader>qs', ':mksession!<CR>', opts 'Make session')
+key('n', '<leader>ql', ':source Session.vim<CR>', opts 'Make load')
 
 -- DAP
-key.set('n', '<leader>db', "<cmd>lua require'dap'.toggle_breakpoint()<cr>", opts 'Toggle [b]reakpoint')
-key.set('n', '<leader>dc', "<cmd>lua require'dap'.continue()<cr>", opts '[c]ontinue')
-key.set('n', '<leader>di', "<cmd>lua require'dap'.step_into()<cr>", opts 'Step [i]nto')
-key.set('n', '<leader>do', "<cmd>lua require'dap'.step_over()<cr>", opts 'Step [o]ver')
-key.set('n', '<leader>dO', "<cmd>lua require'dap'.step_out()<cr>", opts 'Step [O]ut')
-key.set('n', '<leader>dr', "<cmd>lua require'dap'.repl.toggle()<cr>", opts '[r]epl toggle')
-key.set('n', '<leader>dl', "<cmd>lua require'dap'.run_last()<cr>", opts 'Run [l]ast')
-key.set('n', '<leader>dd', "<cmd>lua require'dapui'.toggle()<cr>", opts 'DAP UI Toggle')
-key.set('n', '<leader>dt', "<cmd>lua require'dap'.terminate()<cr>", opts 'DAP [t]errminate')
+key('n', '<leader>db', "<cmd>lua require'dap'.toggle_breakpoint()<cr>", opts 'Toggle [b]reakpoint')
+key('n', '<leader>dc', "<cmd>lua require'dap'.continue()<cr>", opts '[c]ontinue')
+key('n', '<leader>di', "<cmd>lua require'dap'.step_into()<cr>", opts 'Step [i]nto')
+key('n', '<leader>do', "<cmd>lua require'dap'.step_over()<cr>", opts 'Step [o]ver')
+key('n', '<leader>dO', "<cmd>lua require'dap'.step_out()<cr>", opts 'Step [O]ut')
+key('n', '<leader>dr', "<cmd>lua require'dap'.repl.toggle()<cr>", opts '[r]epl toggle')
+key('n', '<leader>dl', "<cmd>lua require'dap'.run_last()<cr>", opts 'Run [l]ast')
+key('n', '<leader>dd', "<cmd>lua require'dapui'.toggle()<cr>", opts 'DAP UI Toggle')
+key('n', '<leader>dt', "<cmd>lua require'dap'.terminate()<cr>", opts 'DAP [t]errminate')
 
 -- Lsp
-key.set('n', '<leader>co', '<cmd>TypescriptOrganizeImports<cr>', opts 'Typescript [o]rganize imports')
-key.set('n', '<leader>cx', '<cmd>TypescriptRemoveUnused<cr>', opts 'Typescript remove unused')
+key('n', '<leader>co', '<cmd>TypescriptOrganizeImports<cr>', opts 'Typescript [o]rganize imports')
+key('n', '<leader>cx', '<cmd>TypescriptRemoveUnused<cr>', opts 'Typescript remove unused')
