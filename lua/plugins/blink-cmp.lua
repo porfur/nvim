ea41298b503dddad997698be2e52a6018d818a73
OP <ovidiu.ps@gmail.com>
2025-07-22 10:29:15 +0300

    Snippets

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,7 +1,47 @@
 return {
   'saghen/blink.cmp',
   -- optional: provides snippets for the snippet source
-  dependencies = { 'rafamadriz/friendly-snippets' },
+    dependencies = {
+      -- Snippet Engine
+      {
+        'L3MON4D3/LuaSnip',
+        version = '2.*',
+        build = (function()
+          -- Build Step is needed for regex support in snippets.
+          -- This step is not supported in many windows environments.
+          -- Remove the below condition to re-enable on windows.
+          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+        config = function()
+          local ls = require 'luasnip'
+          vim.keymap.set({ 'i' }, '<C-s>', function()
+            ls.expand {}
+          end, { silent = true })
+        end,
+      config = function()
+          local ls = require 'luasnip'
+          vim.keymap.set({ 'i' }, '<C-s>', function()
+            ls.expand {}
+          end, { silent = true })
+        end,
+        dependencies = {
+          -- `friendly-snippets` contains a variety of premade snippets.
+          --    See the README about individual language/framework/plugin snippets:
+          --    https://github.com/rafamadriz/friendly-snippets
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
+        },
+        opts = {},
+      },
+      'folke/lazydev.nvim',
+    },
 
   -- use a release tag to download pre-built binaries
   version = '1.*',
@@ -56,7 +96,13 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = {'snippets', 'lsp', 'path',  'buffer' },
+      providers = {
+        lsp = {score_offset=0},
+        snippets = {score_offset=1},
+        path = {score_offset=2},
+        buffer = {score_offset=2}
+      }
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
