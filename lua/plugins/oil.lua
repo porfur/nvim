8a64900bed5e0c27114b9157ce2c5d1d18c5c3a0
OP <ovidiu.ps@gmail.com>
2025-07-23 10:51:55 +0300

    Set oil to verry lazy

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,6 +2,7 @@ return {
   -- https://github.com/stevearc/oil.nvim
     'stevearc/oil.nvim',
     opts = {},
+  event = 'VeryLazy',
   config = function ()
     require('oil').setup({})
     vim.keymap.set('n','<leader>oo',':Oil<CR>',{desc='[o]il'})
