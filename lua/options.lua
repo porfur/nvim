4ca1cfbf6e05c855e92557dbf616a40f4bef9672
OP <ovidiu.ps@gmail.com>
2025-07-23 10:58:26 +0300

    Clean up options

--- lua/options.lua
+++ lua/options.lua
@@ -36,37 +36,15 @@ vim.opt.scrolloff = 5
 vim.opt.sidescrolloff = 5
 vim.opt.foldmethod = 'indent'
 vim.opt.foldenable = false
-vim.opt.path:append("**")  -- enable recursive search
+vim.opt.path:append("**") -- enable recursive search
 
-if vim.g.neovide then
-  -- NOTE: Neovide specific options
-end
+-- if vim.g.neovide then
+-- -- NOTE: Neovide specific options
+-- end
 
 
 vim.diagnostic.config {
   severity_sort = true,
   float = { border = 'rounded', source = 'if_many' },
-  underline = { severity = vim.diagnostic.severity.ERROR },
-  signs = vim.g.have_nerd_font and {
-    text = {
-      [vim.diagnostic.severity.ERROR] = '󰅚 ',
-      [vim.diagnostic.severity.WARN] = '󰀪 ',
-      [vim.diagnostic.severity.INFO] = '󰋽 ',
-      [vim.diagnostic.severity.HINT] = '󰌶 ',
-    },
-  } or {},
   virtual_text = false,
-  -- virtual_text = {
-  --   source = 'if_many',
-  --   spacing = 2,
-  --   format = function(diagnostic)
-  --     local diagnostic_message = {
-  --       [vim.diagnostic.severity.ERROR] = diagnostic.message,
-  --       [vim.diagnostic.severity.WARN] = diagnostic.message,
-  --       [vim.diagnostic.severity.INFO] = diagnostic.message,
-  --       [vim.diagnostic.severity.HINT] = diagnostic.message,
-  --     }
-  --     return diagnostic_message[diagnostic.severity]
-  --   end,
-  -- }
 }
