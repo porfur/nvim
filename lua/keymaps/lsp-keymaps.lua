c7fa42963bce95701fb92a47eb8335c292a531ca
OP <ovidiu.ps@gmail.com>
2025-07-24 13:27:59 +0300

    Update lsp keymaps

--- lua/keymaps/lsp-keymaps.lua
+++ lua/keymaps/lsp-keymaps.lua
@@ -1,5 +1,6 @@
 -- NOTE: This is the callback for the LspAttach autocommand.
 -- Keymaps will be applied only in on the LspAttach event
+--
 return function(event)
   -- NOTE: Remember that Lua is a real programming language, and as such it is possible
   -- to define small helper and utility functions so you don't have to repeat yourself.
@@ -9,6 +10,26 @@ return function(event)
   local map = function(keys, func, desc, mode)
     mode = mode or 'n'
     vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+  end
+
+  --- Returns a function that tries telescope.builtin[name] or falls back to vim.lsp.buf[name]
+  local function lsp_telescope(name)
+    return function()
+      local ok, builtin = pcall(require, 'telescope.builtin')
+      if ok and builtin["lsp_" .. name] then
+        builtin["lsp_" .. name]({
+          initial_mode = 'normal',
+          enble_preview = true,
+        })
+      else
+        local lsp_func = vim.lsp.buf[name]
+        if type(lsp_func) == "function" then
+          lsp_func()
+        else
+          vim.notify("No LSP support for: " .. name, vim.log.levels.ERROR)
+        end
+      end
+    end
   end
 
   -- Rename the variable under your cursor.
@@ -22,21 +43,21 @@ return function(event)
   map('<leader>ca', vim.lsp.buf.code_action, 'Code [a]ction', { 'n', 'x' })
 
   -- Find references for the word under your cursor.
-  map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  map('<leader>cR', require('telescope.builtin').lsp_references, 'Goto [R]eferences')
-  -- map('gr', require('telescope.builtin').lsp_references, '[g]oto [r]eferences')
+  map('grr', lsp_telescope('references'), '[G]oto [R]eferences')
+  map('<leader>cR', lsp_telescope('references'), 'Goto [R]eferences')
+  -- map('gr', lsp_telescope('references'), '[g]oto [r]eferences')
 
   -- Jump to the implementation of the word under your cursor.
   --  Useful when your language has ways of declaring types without an actual implementation.
-  map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  map('<leader>ci', require('telescope.builtin').lsp_implementations, 'Goto [i]mplementation')
+  map('gri', lsp_telescope('implementations'), '[G]oto [I]mplementation')
+  map('<leader>ci', lsp_telescope('implementations'), 'Goto [i]mplementation')
 
   -- Jump to the definition of the word under your cursor.
   --  This is where a variable was first declared, or where a function is defined, etc.
   --  To jump back, press <C-t>.
-  map('grd', require('telescope.builtin').lsp_definitions, '[g]oto [d]efinition')
-  map('gd', require('telescope.builtin').lsp_definitions, '[g]oto [d]efinition')
-  map('<leader>cd', require('telescope.builtin').lsp_definitions, 'Goto [d]efinition')
+  map('grd', lsp_telescope('definitions'), '[g]oto [d]efinition')
+  map('gd', lsp_telescope('definitions'), '[g]oto [d]efinition')
+  map('<leader>cd', lsp_telescope('definitions'), 'Goto [d]efinition')
 
   -- WARN: This is not Goto Definition, this is Goto Declaration.
   --  For example, in C this would take you to the header.
@@ -45,22 +66,19 @@ return function(event)
 
   -- Fuzzy find all the symbols in your current document.
   --  Symbols are things like variables, functions, types, etc.
-  map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
-  map('<leader>cs', require('telescope.builtin').lsp_document_symbols, 'Open Document [s]ymbols')
+  map('gO', lsp_telescope('document_symbols'), 'Open Document Symbols')
+  map('<leader>cs', lsp_telescope('document_symbols'), 'Open Document [s]ymbols')
 
   -- Fuzzy find all the symbols in your current workspace.
   --  Similar to document symbols, except searches over your entire project.
-  map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
-  map('<leader>cw', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace [S}ymbols')
+  map('gW', lsp_telescope('dynamic_workspace_symbols'), 'Open Workspace Symbols')
+  map('<leader>cw', lsp_telescope('dynamic_workspace_symbols'), 'Open Workspace [S}ymbols')
 
   -- Jump to the type of the word under your cursor.
   --  Useful when you're not sure what type a variable is and you want to see
   --  the definition of its *type*, not where it was *defined*.
-  map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
-  map('<leader>ct', require('telescope.builtin').lsp_type_definitions, 'Goto [t]ype Definition')
-
-
-
+  map('grt', lsp_telescope('type_definitions'), '[G]oto [T]ype Definition')
+  map('<leader>ct', lsp_telescope('type_definitions'), 'Goto [t]ype Definition')
 
   --
   -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
@@ -79,9 +97,11 @@ return function(event)
   --
   -- This may be unwanted, since they displace some of your code
   if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
-  -- NOTE: Not working for some reason
+    -- NOTE: Not working for some reason
     map('<leader><Tab>h', function()
-      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+      local state = not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf }
+      print('Inlay Hints', state)
+      vim.lsp.inlay_hint.enable(state)
     end, 'Toggle inlay [h]ints')
   end
 end
